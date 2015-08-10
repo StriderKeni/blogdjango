adcd6dc901cee26d8a5bbaefecddeeb96aed04f3
StriderKeni <ecs.keni@gmail.com>
2015-08-10 09:03:14 -0300

    2.0

--- blogapp/blog/forms.py
+++ blogapp/blog/forms.py
@@ -1,10 +1,16 @@
 __author__ = 'StriderKeni'
 
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class Commentform(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
---
+++ blogapp/blog/migrations/0006_auto_20150810_0145.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0005_auto_20150810_0142'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='created_date',
+            field=models.DateTimeField(default=datetime.datetime(2015, 8, 10, 1, 45, 14, 288527, tzinfo=utc)),
+        ),
+    ]
